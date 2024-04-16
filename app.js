1039382c09ab86bd3c0dd3a04c74ebe798d1dc7f
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-04-16 22:29:59 +0530

    tested layouts

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ app.use(express.static('public'));
 app.use(expressLayouts);
 
 app.set('layout','./layouts/main');
+app.set('view engine',"ejs")
 
 
-// const routes = require('./server/routes/recipeRoutes.js');
-// app.use('/', routes);
+const routes = require('./server/routes/recipeRoutes.js');
+app.use('/', routes);
 
 app.listen(port,()=>{
     console.log(`Listening to port ${port}`);
