505e6b78bef003e7097428cc9c41e2878344f4e7
Sukesh-Hegde <sukeshhegde994591@gmail.com>
2024-04-23 11:24:13 +0530

    updated mongo

--- server/models/database.js
+++ server/models/database.js
@@ -6,10 +6,7 @@ const url = process.env.MONGODB_URI;
 // const url = "mongodb://localhost:27017/RecipeHunt";
 export const connectUsingMongoose = async () => {
   try {
-    await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(url);
     console.log("MongoDB connected using Mongoose");
   } catch (err) {
     console.log("Error while connecting to DB");
