74531306208a5b631914d4178d22c4c834cf3409
Sahil Chhabra <lastspin290901@gmail.com>
2024-04-23 23:49:46 +0530

    file-added

---
+++ server/models/Recipe.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const recipeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: "This field is required.",
+  },
+  description: {
+    type: String,
+    required: "This field is required.",
+  },
+  email: {
+    type: String,
+    required: "This field is required.",
+  },
+  ingredients: {
+    type: Array,
+    required: "This field is required.",
+  },
+  category: {
+    type: String,
+    enum: ["Thai", "American", "Chinese", "Mexican", "Indian"],
+    required: "This field is required.",
+  },
+  image: {
+    type: String,
+    required: "This field is required.",
+  },
+});
+
+recipeSchema.index({ name: "text", description: "text" }); //for the search
+// WildCard Indexing
+//recipeSchema.index({ "$**" : 'text' });
+//recipeSchema.index({ "$**" : 'text' });
+//recipeSchema.index({ "$**" : 'text' });
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+export default Recipe;
